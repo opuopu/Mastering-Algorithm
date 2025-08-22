3308979889e49df242685deaccc72c79ee36f036
opuopu <nurmdopu428@gmail.com>
2025-08-22 19:38:07 +0600

    Update single_source.cpp with latest BFS and path reconstruction

--- single_source.cpp
+++ single_source.cpp
@@ -1,56 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Adjacency list to store the graph
 vector<int> adj_list[1005];
+bool visited[1005];
+int level[1005];
+int parent[1005];
 
-// Visited array to keep track of visited nodes during BFS
-vector<bool> visited(1005, false);
-
-// Level array to store the shortest distance from source to each node
-int level[1005] = {-1};
-
-// Breadth-First Search function to find shortest path from sourceNode to destinationNode
 void bfs(int sourceNode, int destinationNode) {
+  memset(visited, false, sizeof(visited));
+  memset(level, -1, sizeof(level));
+  memset(parent, -1, sizeof(parent));
+
   queue<int> q;
   q.push(sourceNode);
-  level[sourceNode] = 0; // Level of source node is 0
+  level[sourceNode] = 0;
 
-  while(!q.empty()) {
+  while (!q.empty()) {
     int currentNode = q.front();
     q.pop();
-
-    // Skip if already visited
-    if(visited[currentNode]) continue;
-    visited[currentNode] = true;
-
-    // Traverse all neighbors of currentNode
-    for(int neighbor : adj_list[currentNode]) {
-      if(!visited[neighbor]) {
+    if (visited[currentNode]) continue;
+    visited[currentNode] = true;1;
+    for (int neighbor : adj_list[currentNode]) {
+      if (!visited[neighbor]) {
         q.push(neighbor);
-        level[neighbor] = level[currentNode] + 1; // Update level (distance)
+        level[neighbor] = level[currentNode] + 1;
+        parent[neighbor] = currentNode;
       }
     }
   }
 
-  // Output the shortest distance from sourceNode to destinationNode
   cout << level[destinationNode] << endl;
 }
 
 int main() {
   int n, e;
-  cin >> n >> e; // Read number of nodes and edges
+  cin >> n >> e;
 
-  // Read all edges and build the adjacency list
-  while(e--) {
+  while (e--) {
     int u, v;
     cin >> u >> v;
     adj_list[u].push_back(v);
-    adj_list[v].push_back(u); // Since the graph is undirected
+    adj_list[v].push_back(u);
   }
-
-  // Find shortest path from node 0 to node 6 using BFS
   bfs(0, 6);
+  vector<int> path;
+  int node = 6;
+  while (node != -1) {
+    path.push_back(node);
+    node = parent[node];
+  }
+  reverse(path.begin(), path.end());
+  for (int n : path) {
+    cout << n << " ";
+  }
+  cout << endl;
 
   return 0;
 }
